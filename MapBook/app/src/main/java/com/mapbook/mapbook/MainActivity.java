990e6efb647852fa9278569411863e62ceb7a4d5
Haoran Zhang <g090617@gmail.com>
2017-11-02 20:03:11 -0700

    User can sign in now

--- MapBook/app/src/main/java/com/mapbook/mapbook/MainActivity.java
+++ MapBook/app/src/main/java/com/mapbook/mapbook/MainActivity.java
@@ -1,20 +1,85 @@
 package com.mapbook.mapbook;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editEmail = (EditText)findViewById(R.id.editTextEmail);
+        editPassword = (EditText)findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button)findViewById(R.id.buttonSignIn);
+
+        buttonSignIn.setOnClickListener(this);
     }
 
-    public void logIn(View view) {
+    public void logIn() {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    private void userLogIn(){
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Authentication success.",
+                                    Toast.LENGTH_LONG).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            logIn();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }});
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonSignIn){
+            userLogIn();
+        }
     }
 }
