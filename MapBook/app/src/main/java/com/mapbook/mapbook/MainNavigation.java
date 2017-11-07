3e5c93e5a56a22f35d4f3a05fbba77b31effe9b1
dingding951001 <dingding951001@gmail.com>
2017-11-06 22:49:38 -0800

    search location from search page

--- MapBook/app/src/main/java/com/mapbook/mapbook/MainNavigation.java
+++ MapBook/app/src/main/java/com/mapbook/mapbook/MainNavigation.java
@@ -3,12 +3,15 @@ package com.mapbook.mapbook;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,9 +22,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainNavigation extends AppCompatActivity
         implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
@@ -119,6 +128,28 @@ public class MainNavigation extends AppCompatActivity
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
         enableLocation();
+
+        Log.i("enter","before");
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        if (location != null) {
+            Log.i("enter","after");
+            Log.i("enter", location);
+            List<Address> addressList = null;
+            if (location != null || !location.equals("")) {
+                Geocoder geocoder = new Geocoder(this);
+                try {
+                    addressList = geocoder.getFromLocationName(location, 1);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        }
     }
 
     private void enableLocation() {
--- MapBook/app/src/main/java/com/mapbook/mapbook/SearchActivity.java
+++ MapBook/app/src/main/java/com/mapbook/mapbook/SearchActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -18,6 +19,14 @@ public class SearchActivity extends AppCompatActivity {
 
     public void onCancelClick(View v) {
         Intent intent = new Intent(this, MainNavigation.class);
+        startActivity(intent);
+    }
+
+    public void onConfirmClick(View v) {
+        Intent intent = new Intent(this, MainNavigation.class);
+        EditText locationSearch = (EditText) findViewById(R.id.editZipcode);
+        String location = locationSearch.getText().toString();
+        intent.putExtra("location", location);
         startActivity(intent);
     }
 
