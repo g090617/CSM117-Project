b6327fadeff94e9f27e668b66514286101a4bcb6
jmzheng1028 <jmzheng1028@ucla.edu>
2017-11-16 13:36:43 -0800

    View Sale Page

---
+++ MapBook/app/src/main/java/com/mapbook/mapbook/BuyFragment.java
@@ -0,0 +1,106 @@
+package com.mapbook.mapbook;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.widget.ListView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BuyFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BuyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BuyFragment extends ListFragment {
+    String[] books = {
+            "English book",
+            "Math",
+            "Chinese",
+            "Computer Science"
+    };
+   // private OnFragmentInteractionListener mListener;
+
+    public BuyFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static BuyFragment newInstance() {
+        BuyFragment fragment = new BuyFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,books));
+
+    }
+
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id)
+    {
+        Toast.makeText(getActivity(),
+                "You have selected " + books[position],
+                Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_buy, container, false);
+    }
+
+
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+}
